4d743a552c130d376fa475e8d0cfbe61a1c877ce
ashishda <ashish.1.david@gmail.com>
2013-05-28 08:26:04 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fi2app::Application.config.secret_token = '1849cdbf1b6d4b4f44277f62f85b56ad8bf2142ef615a04d920869f961010b81983cb5f1f4660649f865149e0b6cc86ca44376dd9d1bc0ceaecbfd193be0ba0b'
